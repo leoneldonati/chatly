32e7d412b52f06b0d31b6c4e59c22ff229fdc592
Leonel Donati <leonelroman321@gmail.com>
2024-10-01 19:11:40 -0300

    started refactor and added database mongodb atlas

--- src/index.js
+++ src/index.js
@@ -2,30 +2,21 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
 import express from "express";
 import morgan from "morgan";
-import { MongoClient } from "mongodb";
-
+config();
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-
-const url = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(url);
-
-client
-  .connect()
-  .then(() => {
-    console.log("conectado a bdd");
-  })
-  .catch((err) => console.error(err));
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(morgan("dev"));
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
+const url = `mongodb+srv://leonelroman:${process.env.DB_PASS}@cluster0.fo3dmlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(url);
+
+const db = await client.db("we-chat");
+
 
 io.on("connection", (socket) => {
   socket.on("notification", (ntf) => {
@@ -35,6 +26,12 @@ io.on("connection", (socket) => {
   socket.on("message", (msg) => {
     console.log(msg);
   });
+});
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
 server.listen(3000);
