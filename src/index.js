4b309d90a91e0173705f228ef4e8425671841a5f
Leonel Donati <leonelroman321@gmail.com>
2024-10-01 19:37:38 -0300

    added env variables file and refactored

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
 import { MongoClient } from "mongodb";
-import { config } from "dotenv";
 import express from "express";
 import morgan from "morgan";
-config();
+import { port } from "./config.js";
+
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const url = `mongodb+srv://leonelroman:${process.env.DB_PASS}@cluster0.fo3dmlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const client = new MongoClient(url);
-
-const db = await client.db("we-chat");
-
+const client = await new MongoClient(url).connect();
 
 io.on("connection", (socket) => {
   socket.on("notification", (ntf) => {
@@ -34,4 +32,12 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-server.listen(3000);
+server.listen(port, () => console.log({
+  server: {
+    message: 'servidor levantado',
+    port
+  },
+  db: {
+    dbName: client.db('we-chat').databaseName
+  }
+}));
